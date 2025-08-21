69880a775293f5f893afd4f79c2a7d1642470550
RaphaelCastelo <raphaelcb49@gmail.com>
2025-08-21 10:44:04 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Image, View, StyleSheet, SafeAreaView } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Image
+          source={require("./agradecimento.jpeg")
+}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  content: {
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
